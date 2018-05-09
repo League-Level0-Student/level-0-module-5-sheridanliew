1fa9f90ac5ae4b68fa4898d3dfd61ba2573f7c4f
runxue00 <league@wts22.attlocal.net>
2018-05-08 17:00:38 -0700

    Prime or Not Fibonacci (in progress)

---
+++ src/extra/Fibonacci.java
@@ -0,0 +1,18 @@
+package extra;
+
+public class Fibonacci {
+	public static void main(String[] args) {
+		int num1 = 0;
+		int num2 = 1;
+		int sum = 0;
+		System.out.println(num1);
+		System.out.println(num2);
+		for (int i = 0; i < 13; i++) {
+			sum = num1 + num2;
+			System.out.println(sum);
+			num1 = num2;
+			sum = sum + num2;
+			System.out.println(sum);
+		}
+	}
+}
--- src/extra/PrimeOrNot.java
+++ src/extra/PrimeOrNot.java
@@ -4,14 +4,14 @@ import javax.swing.JOptionPane;
 
 public class PrimeOrNot {
 	public static void main(String[] args) {
-		String input = JOptionPane.showInputDialog("Choose any number between 0 and 100.");
+		String input = JOptionPane.showInputDialog("Choose any number between 2 and 100.");
 		int number = Integer.parseInt(input);
 		for (int i = 2; i < number; i++) {
 			if (number % i == 0) {
 				JOptionPane.showMessageDialog(null, "Your number is not a prime number.");
-			} else {
-				JOptionPane.showMessageDialog(null, "Your number is a prime number.");
+				System.exit(0);
 			}
 		}
+		JOptionPane.showMessageDialog(null, "Your number is a prime number.");
 	}
 }
