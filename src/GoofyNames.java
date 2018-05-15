2583744f980ec2f5b371aae4e11aeac17d17ab79
league <league@wts8.attlocal.net>
2018-05-15 16:55:43 -0700

    Fibonacci Goofy Names

--- src/GoofyNames.java
+++ src/GoofyNames.java
@@ -1,3 +1,4 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 0
@@ -10,26 +11,29 @@ public class GoofyNames {
 		String goofyName = "";
 
 		// 1. Ask the user to enter their name
-
+		String name = JOptionPane.showInputDialog("What is your name?");
 		// 2. Print upper case name to the console using .toUpperCase()
-		//    Run your program to see that this works.
-
+		// Run your program to see that this works.
+		System.out.println(name.toUpperCase());
 		// 3. Loop through each character of the name (steps 4 - 6).
-		//    HINT: Use .length() to determine the number of characters in the String.
-
+		// HINT: Use .length() to determine the number of characters in the String.
+		for (int i = 0; i < name.length(); i++) {
 			// 4. Create a String variable to store the next character of the name
-			//    using .substring(start, end)
-			//    HINT: replace 'start' and 'end' to get String with character at i 
-			
+			// using .substring(begin, end)
+			// HINT: replace 'begin' and 'end' to get String with character at i
+			String character = name.substring(i, i + 1);
 			// 5. Use MODULO to set this variable to upper case for EVEN characters
-			//    and lower case for ODD characters.
-
-			
-			// 6. ADD this String  (containing 1 char) to the goofyName String
-
-		
+			// and lower case for ODD characters.
+			if (i % 2 != 0) {
+				character=character.toLowerCase();
+			} else {
+				character=character.toUpperCase();
+			}
+			// 6. ADD this String (containing 1 char) to the goofyName String
+			goofyName= goofyName + character;
+		}
 		// 7. Use pop-up to show user their Goofy name
-
+		JOptionPane.showMessageDialog(null, goofyName);
 	}
-}
 
+}
--- src/extra/Fibonacci.java
+++ src/extra/Fibonacci.java
@@ -4,15 +4,14 @@ public class Fibonacci {
 	public static void main(String[] args) {
 		int num1 = 0;
 		int num2 = 1;
-		int sum = 0;
+		int sum = 1;
 		System.out.println(num1);
 		System.out.println(num2);
-		for (int i = 0; i < 13; i++) {
-			sum = num1 + num2;
+		for (int i = 0; i < 11; i++) {
+			sum=num1+num2;
 			System.out.println(sum);
-			num1 = num2;
-			sum = sum + num2;
-			System.out.println(sum);
+			num1=num2;
+			num2=sum;
 		}
 	}
 }
